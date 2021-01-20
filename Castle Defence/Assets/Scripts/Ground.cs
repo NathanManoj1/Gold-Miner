bb1811e9533f9ca40104e171786b9fa39bcccf5f
Snezel <74683422+Snezel@users.noreply.github.com>
2021-01-20 09:45:24 -0500

    Created jump mechanic and basic mining mechanic.
    Implemented jump mechanic and made the basic mining mechanic.

--- Castle Defence/Assets/Scripts/Ground.cs
+++ Castle Defence/Assets/Scripts/Ground.cs
@@ -1,28 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
+    Player _player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _player = GameObject.Find("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        Debug.Log("Hello");
+        
         if(collision.tag == "Player")
         {
-
-            Destroy(this.gameObject);
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                
+                Destroy(this.gameObject, _player._PowerOfPickaxe);
+            }
+          
         }
     }
+
     
 }
--- Castle Defence/Assets/Scripts/Player.cs
+++ Castle Defence/Assets/Scripts/Player.cs
@@ -7,18 +7,41 @@ public class Player : MonoBehaviour
     Rigidbody2D _rb;
     [SerializeField]
     private float _speed = 1f;
+    [SerializeField]
+    public float _PowerOfPickaxe;
+    [SerializeField]
+    private float _JumpSpeed = 8f;
+    private bool _isTouchingGround;
+    [SerializeField]
+    private LayerMask groundLayer;
+    [SerializeField]
+    private float groundCheckRadius;
+    [SerializeField]
+    private Transform groundCheckPoint;
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
     private void FixedUpdate()
     {
+      
         Movement();
     }
+ 
     void Movement()
     {
+        _isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
         float x = Input.GetAxisRaw("Horizontal");
         Vector2 movement = new Vector2(x, 0);
         _rb.AddForce(_speed * movement);
+        if (Input.GetKeyDown(KeyCode.Space) && _isTouchingGround == true)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _JumpSpeed);
+
+        }
+
+
+
     }
 }
