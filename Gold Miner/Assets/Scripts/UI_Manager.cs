f98c75a99467fa38ca125704d1ed7c0db8bdc03c
Snezel <74683422+Snezel@users.noreply.github.com>
2021-01-29 12:46:49 -0500

    Implemented Timer
    Implemented Timer and made a way to detect when it comes to 0.

--- Gold Miner/Assets/Scripts/UI_Manager.cs
+++ Gold Miner/Assets/Scripts/UI_Manager.cs
@@ -10,10 +10,49 @@ public class UI_Manager : MonoBehaviour
     public int Coins;
     [SerializeField]
     private TextMeshProUGUI _coinText;
- 
+    [SerializeField]
+    private TextMeshProUGUI _Timer;
+    private int _numberofseconds;
+    [SerializeField]
+    private int _numberofminutes = 5;
+
+    private void Start()
+    {
+        _numberofseconds = 60;
+        StartCoroutine(Timer());
+    }
     public void UpdateCoins(int amount)
     {
         Coins += amount;
         _coinText.text = "Coins: " + Coins;
+    }
+    private IEnumerator Timer()
+    {
+        for(int i = 0; i < 70; i++)
+        {
+            yield return new WaitForSeconds(1f);
+            _numberofseconds--;
+            if (_numberofseconds < 10)
+            {
+                _Timer.text = _numberofminutes + ":0" + _numberofseconds;
+            }
+            else
+            {
+                _Timer.text = _numberofminutes + ":" + _numberofseconds;
+            }
+            if (_numberofminutes == 0 && _numberofseconds == 0)
+            {
+                Debug.Log("all done");
+                i = 100;
+                i = 100;
+            }
+            else  if (_numberofseconds == 0)
+            {
+                _numberofseconds = 60;
+                _numberofminutes--;
+                i = 0;
+            }
+        }
+        
     }
 }
